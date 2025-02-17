a2c3cc5476f982a668f7edb0a2ed53de7d4e5212
DeafProger <usvval@yandex.ru>
2025-02-17 13:20:29 +0700

    corrects after comments

--- src/main/java/ru/ifellow/introdution/FirstStep.java
+++ src/main/java/ru/ifellow/introdution/FirstStep.java
@@ -2,135 +2,147 @@ package ru.ifellow.introdution;
 
 public class FirstStep {
 
+    public void tryEmpty(int[] array){
+        if (array.length == 0){
+            throw new IllegalArgumentException("array is empty");
+        };
+    }
+
     public int sum (int x, int y){
-        return x+y;
+        return x + y;
     }
 
     public int mul (int x, int y){
-        return x*y;
+        return x * y;
     }
 
     public int div (int x, int y){
-        return x/y;
+        return x / y;
     }
 
     public int mod (int x, int y){
-        return x%y;
+        return x % y;
     }
 
     public boolean isEqual (int x, int y){
-        return x==y;
+        return x == y;
     }
 
     public boolean isGreater (int x, int y){
-        return x>y;
+        return x > y;
     }
 
     public boolean isInsideRect(int xLeft, int yTop, int xRight, int yBottom, int x, int y){
-        return x>xLeft && x<xRight && y>yBottom && y<yTop;
+        return x > xLeft && x < xRight && y > yBottom && y < yTop;
     }
 
     public int sum(int[] array){
-        int Sum=0;
-        for (int i=0; i<array.length;) Sum+=array[i++];
-        return Sum;
+        int sum = 0;
+        for (int i = 0; i < array.length;) sum += array[i++];
+        return sum;
     }
 
     public int mul(int[] array){
-        int prod=1;
-        for (int i=0; i<array.length;) prod*=array[i++];
+        tryEmpty(array);
+        int prod = 1;
+        for (int i = 0; i < array.length;) prod *= array[i++];
         return prod;
     }
 
     public int min(int[] array){
-        int Min=array[0];
-        for (int i=0; i<array.length;i++) {
-            if (Min > array[i]) Min = array[i];
+        tryEmpty(array);
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) min = array[i];
         }
-        return Min;
+        return min;
     }
 
     public int max(int[] array){
-        int Max=array[0];
-        for (int i=0; i<array.length; i++) {
-            if (Max < array[i]) Max = array[i];
+        tryEmpty(array);
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]) max = array[i];
         }
-        return Max;
+        return max;
     }
 
     public double average(int[] array){
-        return 1.0*sum(array) / array.length;
+        tryEmpty(array);
+        return 1.0 * sum(array) / array.length;
     }
 
     public boolean isSortedDescendant(int[] array){
-    // перевел для себя, что проверяем упорядоченность по убыванию
-        for (int i=1; i<array.length; i++){
-            if (array[i-1]<array[i])return false;
+        for (int i = 1; i < array.length; i++){
+            if (array[i-1] < array[i]) return false;
         }
         return true;
     }
 
     public void cube(int[]array){
-        for (int i=0; i<array.length; i++) array[i] *= array[i]*array[i];
+        for (int i = 0; i < array.length; i++) array[i] *= array[i]*array[i];
     }
 
     public boolean find(int[]array, int value){
-        for (int i=0; i<array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (value == array[i]) return true;
         }
         return false;
     }
 
     public void reverse(int[]array){
-        for (int i=0, k=array.length-1; i<k; i++, k--) {
-            int tmp=array[i];
-            array[i]=array[k];
-            array[k]=tmp;
+        for (int i = 0; i<array.length/2; i++) {
+            int tmp = array[i];
+            array[i] = array[array.length-1-i];
+            array[array.length-1-i] = tmp;
         }
     }
 
     public boolean isPalindrome(int[]array){
-        for (int i=0, k=array.length-1; i<k; i++, k--) {
-            if (array[i] != array[k]) return false;
+        for (int i = 0; i < array.length/2; i++) {
+            if (array[i] != array[array.length-1-i]) return false;
         }
         return true;
     }
 
     public int sum(int[][] matrix){
-        int Sum=0;
-        for (int i=0; i<matrix.length; i++)
-            for (int j=0; j<matrix[i].length; j++)
-                Sum+=matrix[i][j];
-        return Sum;
+        int sum=0;
+        for (int i = 0; i < matrix.length; i++) sum += sum(matrix[i]);
+        return sum;
     }
 
     public int max(int[][] matrix){
-        int Max=matrix[0][0];
-        for (int i=0; i<matrix.length; i++)
-            for (int j=0; j<matrix[i].length; j++) {
-                if (Max < matrix[i][j]) Max = matrix[i][j];
-            }
-        return Max;
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            throw new IllegalArgumentException("matrix is empty");
+        }
+
+        int max = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++){
+            int num = max(matrix[i]);
+            if (max < num) max = num ;
+        }
+
+        return max;
     }
 
     public int diagonalMax(int[][] matrix){
-        int Max=matrix[0][0];
-        for (int i=0; i<matrix.length; i++){
-            if (Max < matrix[i][i]) Max = matrix[i][i];
+        if (matrix.length == 0 || matrix.length != matrix[0].length) {
+            throw new IllegalArgumentException("matrix is no quadratic");
         }
-        return Max;
+        int max=matrix[0][0];
+        for (int i = 0; i < matrix.length; i++){
+            if (max < matrix[i][i]) max = matrix[i][i];
+        }
+        return max;
     }
 
     public boolean isSortedDescendant(int[][] matrix){
-    // так понял, что проверяем упорядоченность каждой строки. Или не так понял?
-        for (int i=0; i<matrix.length; i++)
-            for (int j=1; j<matrix[i].length; j++){
-                if (matrix[i][j-1]<matrix[i][j]) return false;
-        }
+        for (int i = 0; i < matrix.length; i++)
+            if (!isSortedDescendant(matrix[i])) return false;
         return true;
     }
 
-/* for debug
+// for debug
     public static void main(String[] args){
         int x=15, y=6;
         int[] a={1,2,3,4,5};
@@ -140,9 +152,10 @@ public class FirstStep {
                 {3,2,1}
         };
         //System.out.println(isInsideRect(0,20,30,0, x, y));
+        //System.out.println(max(a));
         //cube(a); System.out.println(a[2]);
         //System.out.println(isSortedDescendant(m));
         //System.out.println(diagonalMax(m));
     }
-*/
+
 }
